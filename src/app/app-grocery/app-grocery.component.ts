5428dcbd10722e96b9a922cf8df8a81db24a9a95
mariiapdavey <mariiapdavey@gmail.com>
2024-09-01 22:38:18 -0700

    Created app-grocery component

---
+++ src/app/app-grocery/app-grocery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-grocery',
+  templateUrl: './app-grocery.component.html',
+  styleUrl: './app-grocery.component.css'
+})
+export class AppGroceryComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppGroceryComponent
   ],
   imports: [
     BrowserModule,
