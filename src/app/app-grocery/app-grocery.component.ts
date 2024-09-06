68dbec0962466caf953b8fef0278700ffc123f7b
mariiapdavey <mariiapdavey@gmail.com>
2024-09-06 10:40:37 -0700

    added comments

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ //decorator links the TypeScript code with the HTML and CSS.
   selector: 'app-app-grocery',
   templateUrl: './app-grocery.component.html',
   styleUrl: './app-grocery.component.css'
 })
 export class AppGroceryComponent {
-task: string = ''; // Holds the current task input
-tasks: {name:string}[] = []; // Array of objects with a name property
-
-onClick(){
+task: string = ''; // This is the string that holds the current input from the user.
+tasks: {name:string}[] = []; // This is an array of objects where each object has a name property. 
+                            // Every time a user adds a task, a new object is pushed into this array.
+onClick(){ 
+  //Method: When the user clicks the button, this method:
+//Adds a new task to the tasks array with the name of the task.
+//Clears the input field by resetting the task variable to an empty string ('')
   this.tasks.push({name: this.task});// Push the object
   this.task = '';
 }
